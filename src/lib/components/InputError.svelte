4749b231a0c98578f774cfdf9a876b176543f529
Matias Meno <m@tias.me>
2022-05-31 20:39:38 +0200

    Initial commit

---
+++ src/lib/components/InputError.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	// In your app you must import it like this:
+	// import { getVestContext } from 'use-vest'
+	import { getVestContext } from 'use-vest';
+
+	// The name of the field.
+	export let name: string;
+
+	// All we're interested in here from the context, is the actual Vest
+	// validation result.
+	const { result } = getVestContext();
+
+	// getErrors() returns all errors for all fields. If there are any errors for
+	// a specific field, then it will be in the object as an Array of errors.
+	$: errors = $result.getErrors()[name];
+</script>
+
+<small data-testid={`error-${name}`}>
+	{#if errors}
+		<!--
+			There is no need to display all errors since normally the first
+			error is the most important one (like that this field is required)
+		-->
+		{errors[0]}
+	{/if}
+</small>
+
+<style>
+	small {
+		display: block;
+		color: red;
+		margin: 0.5em 0;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Form from '$lib/components/Form.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Form />
